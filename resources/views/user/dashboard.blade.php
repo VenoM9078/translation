91a166e10e6ec6828e8d9daab39aaa4ea661ffa2
VenoM9078 <roushan.venom1@gmail.com>
2023-01-27 08:08:10 +0500

    Changes

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -1,93 +1,96 @@
 @extends('user.layout')
 
 @section('content')
-    <link href="https://unpkg.com/filepond/dist/filepond.css" rel="stylesheet" />
-    <link href="https://unpkg.com/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css" rel="stylesheet" />
-    <script src="https://unpkg.com/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.js"></script>
-    <script src="https://unpkg.com/filepond-plugin-file-encode/dist/filepond-plugin-file-encode.js"></script>
-    <script src="https://unpkg.com/filepond-plugin-file-validate-size/dist/filepond-plugin-file-validate-size.js"></script>
-    <script src="https://unpkg.com/filepond-plugin-image-exif-orientation/dist/filepond-plugin-image-exif-orientation.js">
-    </script>
-    <script src="https://unpkg.com/filepond/dist/filepond.js"></script>
+<link href="https://unpkg.com/filepond/dist/filepond.css" rel="stylesheet" />
+<link href="https://unpkg.com/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css" rel="stylesheet" />
+<script src="https://unpkg.com/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.js"></script>
+<script src="https://unpkg.com/filepond-plugin-file-encode/dist/filepond-plugin-file-encode.js"></script>
+<script src="https://unpkg.com/filepond-plugin-file-validate-size/dist/filepond-plugin-file-validate-size.js"></script>
+<script src="https://unpkg.com/filepond-plugin-image-exif-orientation/dist/filepond-plugin-image-exif-orientation.js">
+</script>
+<script src="https://unpkg.com/filepond/dist/filepond.js"></script>
 
-    <div class="col-span-12 mt-8">
-        <div class="intro-y flex items-center h-10">
-            <h2 class="text-lg font-medium truncate mr-5">
-                Translation Center
-            </h2>
-        </div>
+<div class="col-span-12 mt-8">
+    <div class="intro-y flex items-center h-10">
+        <h2 class="text-lg font-medium truncate mr-5">
+            Translation Center
+        </h2>
+    </div>
 
-        @if ($message = Session::get('message'))
-            <div class="alert alert-success mt-3">
-                <p>{{ $message }}</p>
-            </div>
-        @endif
+    @if ($message = Session::get('message'))
+    <div class="alert alert-success mt-3">
+        <p>{{ $message }}</p>
+    </div>
+    @endif
 
-        <div class="intro-y box">
-            <div id="vertical-form" class="p-5">
-                <div class="preview">
-                    <div>
-                        <form action="{{ route('user.store') }}" accept-charset="utf-8" method="post"
-                            enctype="multipart/form-data">
-                            @csrf
-                            @method('POST')
-                            <div class="intro-x mt-4">
-                                <input type="text" id="language1" name="language1"
-                                    class="intro-x login__input form-control py-3 px-4 block" required
-                                    placeholder="Current Language of the Document" value="">
-                                <input type="text" id="language2" name="language2"
-                                    class="intro-x login__input form-control py-3 px-4 block mt-4" required
-                                    placeholder="What Language does the Document needs to be Translated into?"
-                                    value="">
-                                <input type="text" name="access_code" id="access_code"
-                                    class="intro-x login__input form-control py-3 px-4 block mt-4"
-                                    placeholder="Access Code (for returning customers)" value="">
-                                <input type="text" name="casemanager" id="casemanager" style="margin-bottom: 20px;"
-                                    class="intro-x login__input form-control py-3 px-4 block mt-4"
-                                    placeholder="Case Manager (optional)" value="">
-                                <input type="file" id="multipleFiles" class="filepond" name="transFiles[]" multiple
-                                    data-max-file-size="10MB" data-max-files="15" />
-                                <div class="flex ">
-                                    <div class="form-check form-switch mb-4">
-                                        <input
-                                            class="form-check-input appearance-none w-9 border-md shadow-lg justify-start border p-2 -ml-10 rounded-full float-left h-5 align-top bg-white bg-no-repeat bg-contain bg-gray-300 focus:outline-none cursor-pointer shadow-sm"
-                                            type="checkbox" role="switch" id="flexSwitchCheckDefault" name="isPayNow">
-                                        <label class="form-check-label inline-block text-black"
-                                            for="flexSwitchCheckDefault">Pay Now</label>
-                                        <br>
-                                        <hr>
-                                    </div>
+    <div class="intro-y box">
+        <div id="vertical-form" class="p-5">
+            <div class="preview">
+                <div>
+                    <form action="{{ route('user.store') }}" accept-charset="utf-8" method="post"
+                        enctype="multipart/form-data">
+                        @csrf
+                        @method('POST')
+                        <div class="intro-x mt-4">
+                            <input type="text" id="language1" name="language1"
+                                class="intro-x login__input form-control py-3 px-4 block" required
+                                placeholder="Current Language of the Document" value="">
+                            <input type="text" id="language2" name="language2"
+                                class="intro-x login__input form-control py-3 px-4 block mt-4" required
+                                placeholder="What Language does the Document needs to be Translated into?" value="">
+                            <input type="text" name="access_code" id="access_code"
+                                class="intro-x login__input form-control py-3 px-4 block mt-4"
+                                placeholder="Access Code (for returning customers)" value="">
+                            <input type="text" name="casemanager" id="casemanager" style="margin-bottom: 20px;"
+                                class="intro-x login__input form-control py-3 px-4 block mt-4"
+                                placeholder="Case Manager (optional)" value="">
+                            <input type="file" id="multipleFiles" class="filepond" name="transFiles[]" multiple
+                                data-max-file-size="10MB" data-max-files="15" />
+                            <hr class="my-2 py-2">
+
+                            <span style="color: rgb(185 28 28); margin-top: 20px; padding-top: 20px;"
+                                class="mt-5 pt-5 text-red-600">
+                                Looking to pay instantly and skip the hassle of waiting for a customized invoice? You
+                                can now Pay Now by enabling the button below.
+                            </span>
+
+                            <div class="flex mt-5 ">
+                                <div class="form-check form-switch mb-4">
+                                    <input
+                                        class="form-check-input appearance-none w-9 border-md shadow-lg justify-start border p-2 -ml-10 rounded-full float-left h-5 align-top bg-white bg-no-repeat bg-contain bg-gray-300 focus:outline-none cursor-pointer shadow-sm"
+                                        type="checkbox" role="switch" id="flexSwitchCheckDefault" name="isPayNow">
+                                    <label class="form-check-label inline-block text-black"
+                                        for="flexSwitchCheckDefault">Pay Now</label>
+                                    <br>
+                                    <hr>
                                 </div>
-                                <span class="m-1 font-bold">
-                                    By clicking 'Pay Now', you agree to our terms and conditions and authorize the
-                                    charge to your credit card.
-                                </span>
-
                             </div>
 
-                            <div class="btn-group mt-5" role="group" aria-label="Basic example" style="margin-top: 3rem;">
+                        </div>
 
-                                <button type="submit" id="uploadBtn" class="btn btn-primary ml-3">Upload Files for
-                                    Translation</button>
-                            </div>
+                        <div class="btn-group mt-5" role="group" aria-label="Basic example">
 
-                        </form>
-                    </div>
+                            <button type="submit" id="uploadBtn" class="btn btn-primary">Upload Files for
+                                Translation</button>
+                        </div>
+
+                    </form>
                 </div>
-
             </div>
+
         </div>
     </div>
-    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
-        crossorigin="anonymous"></script>
-    <!-- add before </body> -->
-    {{-- TODO:CHANGE Client id --}}
-    <script
-        src="https://www.paypal.com/sdk/js?client-id=AapYCwr7IL6pstdnEZ8a8Ugv_WMX3qBJflHAfrlFwye5D-7oB22i8Nrky2_AwRLLLTayYkhWS21uKygn&disable-funding=credit&components=buttons">
-    </script>
+</div>
+<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
+    crossorigin="anonymous"></script>
+<!-- add before </body> -->
+{{-- TODO:CHANGE Client id --}}
+<script
+    src="https://www.paypal.com/sdk/js?client-id=AapYCwr7IL6pstdnEZ8a8Ugv_WMX3qBJflHAfrlFwye5D-7oB22i8Nrky2_AwRLLLTayYkhWS21uKygn&disable-funding=credit&components=buttons">
+</script>
 
-    <script>
-        if ($("#flexSwitchCheckDefault").is(':checked') == false) {
+<script>
+    if ($("#flexSwitchCheckDefault").is(':checked') == false) {
             $("#paypal-button-container").hide();
         } else {
             $("#paypal-button-container").show();
@@ -123,5 +126,5 @@
 
 
         order = 4
-    </script>
+</script>
 @endsection
