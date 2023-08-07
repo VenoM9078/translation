4bbd9e61cd9fb17faabd12ca7e92aed1cede0dec
Roushan <wahajdorift@yahoo.com>
2023-08-07 11:13:48 +0500

    added dd

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2253,6 +2253,7 @@ class AdminController extends Controller
         } else {
             $validated['proofreader_id'] = -1;
         }
+        dd($validated);
         $completedRequest = CompletedRequest::create($validated);
 
         Order::where('id', $order_id)->update([
